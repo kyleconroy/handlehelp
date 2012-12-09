577bb46622f2ef0c7e0d1a7cb0b25b593e0b1cba
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-12-09 06:25:36 -0800

    Add 5 more services

--- handlehelp.go
+++ handlehelp.go
@@ -10,44 +10,34 @@ import (
 "regexp"
 )
 
-func reddit(handle string) (string, bool) {
-
-	valid, err := regexp.MatchString("^[a-zA-Z0-9_-]{3,20}$", handle)
-
-	if !valid || err != nil {
-		return "reddit", false
-	}
-
-	res, err := http.Get("http://www.reddit.com/user/" + handle)
-	
-	if err != nil {
-		return "reddit", false
-	}
-	
-	return "reddit", res.StatusCode == 404
-}
-
-func twitter(handle string) (string, bool) {
-
-	valid, err := regexp.MatchString("^[a-zA-Z0-9_]{1,15}$", handle)
-
-	if !valid || err != nil {
-		return "twitter", false
-	}
-
-	res, err := http.Get("http://www.twitter.com/" + handle)
-	
-	if err != nil {
-		return "twitter", false
-	}
-
-	return "twitter", res.StatusCode == 404
+type Website struct {
+	Pattern string
+	UserURL string
+	Name string
 }
 
 type handleResult struct {
 	Site      string
 	Available bool
 }
+
+func checkHandle(handle string, site Website) (bool) {
+
+	valid, err := regexp.MatchString(site.Pattern, handle)
+
+	if !valid || err != nil {
+		return false
+	}
+
+	res, err := http.Get(fmt.Sprintf(site.UserURL, handle))
+	
+	if err != nil {
+		return false
+	}
+	
+	return res.StatusCode == 404
+}
+
 
 func main() {
 	tmpl, err := template.ParseFiles("index.html")
@@ -56,7 +46,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	sites := []func(string) (string, bool){reddit, twitter}
+	sites := []Website{
+		Website{Name: "twitter", UserURL: "http://twitter.com/%s", Pattern: "^[a-zA-Z0-9_]{1,15}$"},
+		Website{Name: "reddit", UserURL: "http://reddit.com/user/%s", Pattern: "^[a-zA-Z0-9_-]{3,20}$"},
+		Website{Name: "hacker news", UserURL: "http://news.ycombinator.com/user?id=%s", Pattern: "^[a-zA-Z0-9_-]{2,15}$"},
+		Website{Name: "lobsters", UserURL: "https://lobste.rs/u/%s", Pattern: "^[a-zA-Z0-9_-]{2,20}$"},
+		Website{Name: "dribble", UserURL: "https://dribble.com/%s", Pattern: "^[a-zA-Z0-9_-]{2,20}$"},
+		Website{Name: "forrst", UserURL: "https://forrst.com/people/%s", Pattern: "^[a-zA-Z0-9_]{1,20}$"},
+		Website{Name: "facebook", UserURL: "https://facebook.com/%s", Pattern: "^[a-zA-Z0-9\\.]{3,50}$"},
+		Website{Name: "youtube", UserURL: "https://www.youtube.com/user/%s", Pattern: "^[a-zA-Z0-9]{3,50}$"},
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, nil)
@@ -76,11 +75,11 @@ func main() {
 
 		cm := make(chan handleResult)
 
-		for _, checkHandle := range sites {
-			go func(ch func(string) (string, bool)) {
-				s, a := ch(r.FormValue("handle"))
-				cm <- handleResult{Site: s, Available: a}
-			}(checkHandle)
+		for _, site := range sites {
+			go func(site Website) {
+				a := checkHandle(r.FormValue("handle"), site)
+				cm <- handleResult{Site: site.Name, Available: a}
+			}(site)
 		}
 
 		for _, _ = range sites {
